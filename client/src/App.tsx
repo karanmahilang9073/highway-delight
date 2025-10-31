299a03ec94f98c1302fde7bc1f7fdde6b5eb1c7d
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2025-10-31 10:46:57 +0530

    unnecessary code reoved

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 // 1. Import useState
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Details from './pages/Details';
