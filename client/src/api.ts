fb541cfd24b6b0e176dab5df32a53471cb4ad7a4
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2025-10-31 10:36:30 +0530

    api updated

--- client/src/api.ts
+++ client/src/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // 1. We create a new 'instance' of axios
 const api = axios.create({
   // 2. We set the base URL for ALL API requests
-  baseURL: 'http://localhost:5000', 
+  baseURL: 'https://highway-delight-2.onrender.com', 
 });
 
 // 3. When you deploy your site, you will change this one line to:
